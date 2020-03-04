8f3582b77fdfc90de7b5d8af142ce441c806b474
Wes Bos <wesbos@gmail.com>
2020-03-04 14:19:15 -0500

    Tags and slugs

--- tagify.js
+++ tagify.js
@@ -1,16 +1,30 @@
 const glob = require('glob');
+const { promises: fs } = require('fs');
 
 async function go() {
-  glob('./src/tips/**/*.mdx', (err, files) => {
+  glob('./src/tips/**/*.mdx', async (err, files) => {
     for (const path of files) {
       const [, , , folderName, fileName] = path.split('/');
 
       const [slug, tagStart] = folderName.split('[');
-      if (!tagStart) {
-        console.log(`No tags for ${folderName}`);
-      }
-      // const tags = tagStart.split(',');
-      // console.log({ folderName, fileName, slug, tags });
+      const tags = tagStart.replace(']', '').split(',');
+      const content = await fs.readFile(path, 'utf-8');
+      console.log({ folderName, fileName, slug, tags });
+      const formattedTags = `tags:
+${tags.map(tag => `  - ${tag}`).join('\n')}`;
+      let updatedContent = content.replace(
+        '---',
+        `---
+${formattedTags}`
+      );
+
+      const [existingSlug] = content.match(/slug: .*/);
+
+      updatedContent = updatedContent.replace(existingSlug, `slug: ${slug}`);
+      // console.log(existingSlug);
+
+      // console.log(updatedContent);
+      await fs.writeFile(path, updatedContent, 'utf-8');
     }
   });
 }
