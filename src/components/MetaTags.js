abe77a78d5f3cb3f40188380532741852d1c651e
Wes Bos <wesbos@gmail.com>
2020-03-13 12:22:21 -0400

    absolute urls

--- src/components/MetaTags.js
+++ src/components/MetaTags.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 export function PostMetaTags({ post }) {
-  console.log(process.env.DEPLOY_PRIME_URL);
   const canonical = `https://wesbos.com/${post.frontmatter.slug}`;
-  const url = `${process.env.URL}/${post.frontmatter.slug}`;
+  const url = `/${post.frontmatter.slug}`;
   const thumbnailData = {
     title: post.frontmatter.title,
     url,
@@ -16,7 +15,7 @@ export function PostMetaTags({ post }) {
     )
   ).toString();
 
-  const ogImage = `${process.env.URL}/.netlify/functions/thumbnail?${thumbnailQuery}`;
+  const ogImage = `/.netlify/functions/thumbnail?${thumbnailQuery}`;
   return (
     <Helmet>
       <link rel="canonical" href={canonical} />
@@ -47,9 +46,9 @@ export function PostMetaTags({ post }) {
 export function TipMetaTags({ post }) {
   const location = useLocation();
   const canonical = `https://wesbos.com/${post.frontmatter.slug}`;
-  const url = `${process.env.URL}/${post.frontmatter.slug}`;
+  const url = `/${post.frontmatter.slug}`;
   const thumbnailQuery = `?title=${post.frontmatter.title}&url=${url}&thumbnail=${post.frontmatter.image.publicURL}`;
-  const ogImage = `${process.env.URL}/.netlify/functions/thumbnail${thumbnailQuery}`;
+  const ogImage = `/.netlify/functions/thumbnail${thumbnailQuery}`;
   return (
     <Helmet>
       <link rel="canonical" href={canonical} />
