bb16ea9cf8e18966f4225ebc76da6fe6099682f7
Wes Bos <wesbos@gmail.com>
2020-04-17 13:12:59 -0400

    hmm

--- functions/isnetlify.js
+++ functions/isnetlify.js
@@ -1,5 +1,11 @@
 exports.handler = async (event, context) => ({
   statusCode: 200,
-  body: process.env.NETLIFY,
-  isBase64Encoded: true,
+  body: JSON.stringify({
+    NETLIFY: process.env.NETLIFY,
+    BUILD_ID: process.env.BUILD_ID,
+    CONTEXT: process.env.CONTEXT,
+    NODE_VERSION: process.env.NODE_VERSION,
+    URL: process.env.URL,
+    DEPLOY_URL: process.env.DEPLOY_URL,
+  }),
 });
