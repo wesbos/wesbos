afeb5d01058c47664895863b22e1f97ea29bb24c
Wes Bos <wesbos@gmail.com>
2020-09-24 11:21:53 -0400

    I dont even know if this will fix it

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,6 +33,7 @@ async function makePostsFromMdx({ graphql, actions }) {
     `
   );
   if (errors) {
+    console.log(errors);
     throw new Error('There was an error');
   }
   const posts = data.allMdx.edges;
@@ -75,6 +76,7 @@ async function makeTipsFromMdx({ graphql, actions }) {
     `
   );
   if (errors) {
+    console.log(errors);
     throw new Error('There was an error');
   }
   const tips = data.allMdx.edges;
@@ -113,6 +115,7 @@ async function paginate({
     `
   );
   if (errors) {
+    console.log(errors);
     throw new Error('There was an error');
   }
   const { totalCount } = data.allMdx;
