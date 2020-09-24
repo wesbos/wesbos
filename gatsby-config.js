274e2a966716d8776cc8435b298bccc937e77ce7
Wes Bos <wesbos@gmail.com>
2020-09-24 11:34:05 -0400

    hmm

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,14 +10,15 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-remove-trailing-slashes`,
-    {
-      resolve: `gatsby-plugin-prettier-build`,
-      options: {
-        types: ['html'],
-        concurrency: 20,
-        verbose: true,
-      },
-    },
+    // taking this off for a bit because its broken or choking on somthing...
+    // {
+    //   resolve: `gatsby-plugin-prettier-build`,
+    //   options: {
+    //     types: ['html'],
+    //     concurrency: 20,
+    //     verbose: true,
+    //   },
+    // },
     {
       // This tells us where the plugin lives
       // this one is in our node_modules
