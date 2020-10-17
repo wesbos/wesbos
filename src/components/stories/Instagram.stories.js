30af0eb1af382b553c7c3c257cbac61af2c6308f
Roberto Sousa <beto.sousa22@gmail.com>
2020-10-17 01:49:16 +0100

    writwe basic stories

---
+++ src/components/stories/Instagram.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Instagram from '../Instagram';
+
+export default {
+  title: 'components/Instagram',
+  component: Instagram,
+};
+
+const Component = () => <Instagram />;
+
+export const Default = Component.bind({});
