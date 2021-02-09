4ce58b0b648fa5de716adaa0be52b8eefd440b29
Wes Bos <wesbos@gmail.com>
2021-02-09 10:01:30 -0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,6 +143,7 @@ module.exports = {
       options: {
         apiKey: process.env.GATSBY_SNIPCART_APIKEY,
         currency: 'usd',
+        // Upgrade to latest snipcart
         js: `https://cdn.snipcart.com/themes/v3.0.28/default/snipcart.js`,
         css: `https://cdn.snipcart.com/themes/v3.0.28/default/snipcart.css`,
       },
