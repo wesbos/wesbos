efbd32e4d0c2c28ebb7a410de5d09aa4300b80da
Wes Bos <wesbos@gmail.com>
2021-02-16 14:35:34 -0500

    swag less than 0

--- src/components/store/Product.js
+++ src/components/store/Product.js
@@ -32,7 +32,7 @@ export default function Product({ product, buttonAttrs }) {
               className="snipcart-add-item"
               {...buttonAttrs}
               data-item-custom1-value={variant.variation[0].option}
-              disabled={variant.stock === 0}
+              disabled={variant.stock <= 0}
             >
               <span className="variantName">
                 {variant.variation
