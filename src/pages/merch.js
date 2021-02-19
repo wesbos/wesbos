7235e0ae7681503407ed51634bcf77e6af7f883d
Wes Bos <wesbos@gmail.com>
2021-02-19 12:15:26 -0500

    opacity

--- src/pages/merch.js
+++ src/pages/merch.js
@@ -22,7 +22,7 @@ export default function SwagPage({ data, path }) {
   const { products } = useSnipCartProducts();
   const { getImagePath } = useImage();
   return (
-    <div>
+    <div stlye={{ opacity: 0 }}>
       <H as="h2">Merch Store</H>
       <PostMetaTags
         post={{
