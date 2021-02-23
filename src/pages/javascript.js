56e50e2a85d85c43dc4447fbf67dbdc404c64bd5
A-J Roos <asjas@hey.com>
2021-02-23 20:08:22 +0200

    add toc title to each post and sort posts on main javascript page

--- src/pages/javascript.js
+++ src/pages/javascript.js
@@ -7,16 +7,19 @@ export default function JavaScriptPage({ data: { allMdx: javascript }, path }) {
   return (
     <>
       <H>Beginner JavaScript</H>
-      <p>This is a resource for the Beginner JavaScript course.</p>
+      <p>
+        This is a resource for the Beginner JavaScript course that contains
+        notes and extra tidbits.
+      </p>
       <H as="h2">Table of Contents</H>
 
       <div>
         {javascript.nodes.map((item) => (
           <a
-            key={item.frontmatter.title}
+            key={item.frontmatter.tocTitle}
             href={`/javascript/${item.frontmatter.slug}`}
           >
-            <H as="h3">{item.frontmatter.title}</H>
+            <H as="h4">{item.frontmatter.tocTitle}</H>
           </a>
         ))}
       </div>
@@ -35,10 +38,13 @@ export default function JavaScriptPage({ data: { allMdx: javascript }, path }) {
 
 export const pageQuery = graphql`
   query JavaScript {
-    allMdx(filter: { fields: { collection: { eq: "javascript" } } }) {
+    allMdx(
+      filter: { fields: { collection: { eq: "javascript" } } }
+      sort: { fields: frontmatter___tocTitle }
+    ) {
       nodes {
         frontmatter {
-          title
+          tocTitle
           slug
         }
       }
