f104e0e52a1dc6758b68f28e0ed2cca7c490a54c
A-J Roos <asjas@hey.com>
2021-02-25 15:38:05 +0200

    create aside toc with beginner js notes styles

---
+++ src/components/styles/JavaScriptNotesStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const JavaScriptNotesStyles = styled.div`
+  display: grid;
+  grid-template-columns: 60ch;
+  grid-template-areas: 'post';
+
+  @media (min-width: 1600px) {
+    grid-template-columns: minmax(240px, 400px) 60ch 1fr;
+    position: relative;
+    grid-template-areas: 'toc post aside';
+    gap: 6rem;
+  }
+`;
+
+export default JavaScriptNotesStyles;
--- src/templates/beginner-javascript.js
+++ src/templates/beginner-javascript.js
@@ -5,8 +5,10 @@ import { IoLogoGithub } from 'react-icons/io';
 import { Helmet } from 'react-helmet';
 import H from '../components/mdxComponents/Headings';
 import ContentNav from '../components/ContentNav';
+import AsideTableOfContents from '../components/AsideTableOfContents';
 import PostHeaderStyles from '../components/styles/PostHeaderStyles';
 import EditDialogStyles from '../components/styles/EditDialogStyles';
+import JavaScriptNotesStyles from '../components/styles/JavaScriptNotesStyles';
 import { PostMetaTags } from '../components/MetaTags';
 
 export const pageQuery = graphql`
@@ -19,21 +21,19 @@ export const pageQuery = graphql`
     }
     mdx(fields: { slug: { eq: $slug } }) {
       id
-      excerpt
       fileAbsolutePath
       frontmatter {
         title
         slug
         date(formatString: "MMMM DD, YYYY")
-        image {
-          ...ImageFields
-        }
         category
       }
+      tableOfContents
       body
     }
   }
 `;
+
 function PostTemplate({ data: { mdx: post }, scope, pageContext }) {
   if (!post) {
     return <p>No Post Found? This should be a 404</p>;
@@ -43,49 +43,59 @@ function PostTemplate({ data: { mdx: post }, scope, pageContext }) {
   }`;
 
   return (
-    <>
-      <PostHeaderStyles>
-        <PostMetaTags post={post} />
-        <H>{post.frontmatter.title}</H>
-        <div className="postMeta">
-          <time dateTime={post.frontmatter.date}>{post.frontmatter.date}</time>
-          <span>{post.frontmatter.category.join(', ')}</span>
-          <a rel="noopener noreferrer" target="_blank" href={editURL}>
-            Edit Post <IoLogoGithub />
-          </a>
-        </div>
-      </PostHeaderStyles>
-      <MDXRenderer
-        scope={{
-          ...scope,
-        }}
-      >
-        {post.body}
-      </MDXRenderer>
-      <EditDialogStyles>
-        <p>
-          Find an issue with this post? Think you could clarify, update or add
-          something?
-        </p>
-        <p>
-          All my posts are available to edit on Github. Any fix, little or
-          small, is appreciated!
-        </p>
-        <p>
-          <a rel="noopener noreferrer" target="_blank" href={editURL}>
-            <IoLogoGithub /> Edit on Github
-          </a>
-        </p>
-      </EditDialogStyles>
-      <ContentNav
-        pathPrefix={pageContext.pathPrefix}
-        prev={pageContext.prev}
-        next={pageContext.next}
-      />
-      <Helmet>
-        <title>{post.frontmatter.title} - Beginner JavaScript - Wes Bos</title>
-      </Helmet>
-    </>
+    <JavaScriptNotesStyles>
+      <div>
+        <p>Replace with Beginner JS TOC</p>
+      </div>
+      <div>
+        <PostHeaderStyles>
+          <PostMetaTags post={post} />
+          <H>{post.frontmatter.title}</H>
+          <div className="postMeta">
+            <time dateTime={post.frontmatter.date}>
+              {post.frontmatter.date}
+            </time>
+            <span>{post.frontmatter.category.join(', ')}</span>
+            <a rel="noopener noreferrer" target="_blank" href={editURL}>
+              Edit Post <IoLogoGithub />
+            </a>
+          </div>
+        </PostHeaderStyles>
+        <MDXRenderer
+          scope={{
+            ...scope,
+          }}
+        >
+          {post.body}
+        </MDXRenderer>
+        <EditDialogStyles>
+          <p>
+            Find an issue with this post? Think you could clarify, update or add
+            something?
+          </p>
+          <p>
+            All my posts are available to edit on Github. Any fix, little or
+            small, is appreciated!
+          </p>
+          <p>
+            <a rel="noopener noreferrer" target="_blank" href={editURL}>
+              <IoLogoGithub /> Edit on Github
+            </a>
+          </p>
+        </EditDialogStyles>
+        <ContentNav
+          pathPrefix={pageContext.pathPrefix}
+          prev={pageContext.prev}
+          next={pageContext.next}
+        />
+        <Helmet>
+          <title>
+            {post.frontmatter.title} - Beginner JavaScript - Wes Bos
+          </title>
+        </Helmet>
+      </div>
+      <AsideTableOfContents tableOfContents={post.tableOfContents} />
+    </JavaScriptNotesStyles>
   );
 }
 
