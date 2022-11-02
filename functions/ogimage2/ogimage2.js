9194242fc5ea3ab0ba8285a5906a50c3d8f51aeb
Wes Bos <wesbos@gmail.com>
2022-11-01 20:58:24 -0400

    remove

--- functions/ogimage2/ogimage2.js
+++ functions/ogimage2/ogimage2.js
@@ -1,68 +1,68 @@
-// Switch to this, re: https://answers.netlify.com/t/netlify-function-with-puppeteer-breaks-if-i-make-any-changes/76924/8
-const chrome = require('@sparticuz/chromium');
+// // Switch to this, re: https://answers.netlify.com/t/netlify-function-with-puppeteer-breaks-if-i-make-any-changes/76924/8
+// const chrome = require('@sparticuz/chromium');
 
-//     "chrome-aws-lambda": "^v2.1.1",
-// "puppeteer-core": "^2.1.0",
+// //     "chrome-aws-lambda": "^v2.1.1",
+// // "puppeteer-core": "^2.1.0",
 
-const puppeteer = require('puppeteer-core');
-const wait = require('waait');
+// const puppeteer = require('puppeteer-core');
+// const wait = require('waait');
 
-const cached = new Map();
+// const cached = new Map();
 
-const exePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+// const exePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
-async function getOptions(isDev) {
-  if (isDev) {
-    return {
-      product: 'chrome',
-      args: [],
-      executablePath: exePath,
-      headless: true,
-    };
-  }
-  return {
-    product: 'chrome',
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  };
-}
+// async function getOptions(isDev) {
+//   if (isDev) {
+//     return {
+//       product: 'chrome',
+//       args: [],
+//       executablePath: exePath,
+//       headless: true,
+//     };
+//   }
+//   return {
+//     product: 'chrome',
+//     args: chrome.args,
+//     executablePath: await chrome.executablePath,
+//     headless: chrome.headless,
+//   };
+// }
 
-async function getScreenshot(url, isDev) {
-  console.log({ isDev, url: process.env.URL });
-  // first check if this value has been cached
-  const cachedImage = cached.get(url);
-  if (cachedImage) {
-    return cachedImage;
-  }
-  const options = await getOptions(isDev);
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 1.5 });
-  await page.goto(url);
-  await wait(1000);
-  const buffer = await page.screenshot({ type: 'png' });
-  const base64Image = buffer.toString('base64');
-  cached.set(url, base64Image);
-  return base64Image;
-}
+// async function getScreenshot(url, isDev) {
+//   console.log({ isDev, url: process.env.URL });
+//   // first check if this value has been cached
+//   const cachedImage = cached.get(url);
+//   if (cachedImage) {
+//     return cachedImage;
+//   }
+//   const options = await getOptions(isDev);
+//   const browser = await puppeteer.launch(options);
+//   const page = await browser.newPage();
+//   await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 1.5 });
+//   await page.goto(url);
+//   await wait(1000);
+//   const buffer = await page.screenshot({ type: 'png' });
+//   const base64Image = buffer.toString('base64');
+//   cached.set(url, base64Image);
+//   return base64Image;
+// }
 
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
-  const qs = new URLSearchParams(event.queryStringParameters);
-  console.log(
-    `${process.env.URL || `http://localhost:8888`}/thumbnail?${qs.toString()}`
-  );
-  const photoBuffer = await getScreenshot(
-    `${process.env.URL || `http://localhost:8888`}/thumbnail?${qs.toString()}`,
-    // Here we need to pass a boolean to say if we are on the server. Netlify has a bug where process.env.NETLIFY is undefiend in functions so I'm using one of the only vars I can find
-    // !process.env.NETLIFY
-    process.env.URL.includes('http://localhost')
-  );
-  console.log('x');
-  return {
-    statusCode: 200,
-    body: photoBuffer,
-    isBase64Encoded: true,
-  };
-};
+// // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+// exports.handler = async (event, context) => {
+//   const qs = new URLSearchParams(event.queryStringParameters);
+//   console.log(
+//     `${process.env.URL || `http://localhost:8888`}/thumbnail?${qs.toString()}`
+//   );
+//   const photoBuffer = await getScreenshot(
+//     `${process.env.URL || `http://localhost:8888`}/thumbnail?${qs.toString()}`,
+//     // Here we need to pass a boolean to say if we are on the server. Netlify has a bug where process.env.NETLIFY is undefiend in functions so I'm using one of the only vars I can find
+//     // !process.env.NETLIFY
+//     process.env.URL.includes('http://localhost')
+//   );
+//   console.log('x');
+//   return {
+//     statusCode: 200,
+//     body: photoBuffer,
+//     isBase64Encoded: true,
+//   };
+// };
