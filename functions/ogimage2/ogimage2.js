9e07012880fb937b22b53c7c865c8ba44fd4bf41
Wes Bos <wesbos@gmail.com>
2022-11-01 21:18:39 -0400

    oops

--- functions/ogimage2/ogimage2.js
+++ functions/ogimage2/ogimage2.js
@@ -66,3 +66,15 @@
 //     isBase64Encoded: true,
 //   };
 // };
+
+exports.handler = async (event, context) => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    NETLIFY: process.env.NETLIFY,
+    BUILD_ID: process.env.BUILD_ID,
+    CONTEXT: process.env.GATSBY_CONTEXT,
+    NODE_VERSION: process.env.NODE_VERSION,
+    URL: process.env.URL,
+    DEPLOY_URL: process.env.DEPLOY_URL,
+  }),
+});
