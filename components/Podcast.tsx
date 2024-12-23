d9f52cf4e6ee5214aabbe4b5e2fa4b30059882a2
Wes Bos <wesbos@gmail.com>
2024-12-23 10:39:02 -0500

    replace public imports with public strings

--- components/Podcast.tsx
+++ components/Podcast.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 const url = `https://syntax.fm/api/shows/latest`;
-import logo from '@/public/images/syntax-logo.jpg';
 import { FooterBlock, FooterHeading } from '@/styles/FooterStyles.module.css';
 
 export default async function Podcast() {
@@ -13,11 +12,15 @@ export default async function Podcast() {
       <h3 className={FooterHeading}>
         <span className="highlight">Syntax Podcast: #{podcast.number}</span>
       </h3>
-      <Image width="500" alt="Syntax Podcast" src={logo} />
-      <time style={{
+      <Image width={500} height={500} alt="Syntax Podcast" src="/images/syntax-logo.jpg" />
+      <time
+        style={{
           fontSize: '1.2rem',
           fontWeight: '600',
-        }}>{podcast.displayDate}</time>
+        }}
+      >
+        {podcast.displayDate}
+      </time>
       <p>{podcast.title}</p>
       <a href={`https://syntax.fm${podcast.slug}`}>Listen Now → </a>
     </div>
