bfefb587a778a4bab8b72d1c3a74f7b8b1468857
Wes Bos <wesbos@gmail.com>
2025-01-10 16:54:07 -0500

    WIP metadata

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -6,8 +6,8 @@ import { postMeta } from '@/styles/PostMeta.module.css';
 import Image, { ImageProps } from 'next/image';
 import { IoLogoGithub } from 'react-icons/io';
 
-export default async function BlogPost({ params, children }: { params: { slug: string, section: string }, children: React.ReactNode }) {
-  const { slug, section } = await params;
+export default async function BlogPost({ params, children }: { params: { slug: string }, children: React.ReactNode }) {
+  const { slug } = await params;
   const post = await getPostBySlug(slug);
   if (!post) {
     return <p>Post not found</p>;
@@ -56,4 +56,41 @@ export default async function BlogPost({ params, children }: { params: { slug: s
 export async function generateStaticParams() {
   const { posts } = await getPosts();
   return posts.map((post) => ({ slug: post.frontmatter.slug }));
+}
+
+// or Dynamic metadata
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const { slug } = await params;
+  const post = await getPostBySlug(slug);
+  if (!post) {
+    return {
+      title: 'Post not found',
+    };
+  }
+
+  const url = `https://wesbos.com/${post.frontmatter.slug}`;
+  /*
+      <link rel="canonical" href={canonical} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@wesbos" />
+      <meta name="twitter:creator" content="@wesbos" />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:title" content={post.frontmatter.title} />
+      <meta name="twitter:description" content={post.excerpt} />
+      <meta name="twitter:image" content={ogImage} />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={post.frontmatter.title} />
+      <meta property="og:url" content={url} />
+      <meta property="og:description" content={post.excerpt} />
+      {post.frontmatter.date ? <meta property="article:published_time" content={new Date(post.frontmatter.date).toISOString()} /> : null}
+      <meta property="og:site_name" content="Wes Bos" />
+      <meta property="og:image" content={ogImage} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:locale" content="en_CA" />
+      <title>{`${post.frontmatter.title} - Wes Bos`}</title>
+  */
+  return {
+
+  }
 }
