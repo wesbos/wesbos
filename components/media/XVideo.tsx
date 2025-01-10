6659117477cc106d6c532a217d73dee3af993927
Wes Bos <wesbos@gmail.com>
2025-01-10 12:21:33 -0500

    disable hls video element as its too large for cloudflare workers.. for now

--- components/media/XVideo.tsx
+++ components/media/XVideo.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { MediaController, MediaControlBar, MediaTimeRange, MediaTimeDisplay, MediaVolumeRange, MediaPlayButton, MediaMuteButton, MediaPlaybackRateButton } from 'media-chrome/react';
-import HLSVideoElement from 'hls-video-element/react';
+// import HLSVideoElement from 'hls-video-element/react';
 import { XVideoVariant } from '@/lib/socials/twitter-fetcher';
 import { MediaSettingsMenu, MediaSettingsMenuButton, MediaSettingsMenuItem, MediaRenditionMenu } from 'media-chrome/react/menu';
 export function XVideoPlayer({ url, contentType, style }: { url: string; contentType: XVideoVariant['content_type']; style: React.CSSProperties }) {
@@ -8,9 +8,12 @@ export function XVideoPlayer({ url, contentType, style }: { url: string; content
     <div style={style}>
       <MediaController>
         {contentType === 'application/x-mpegURL' ? (
-          <HLSVideoElement onErrorCapture={(err) => {
+          <div>
+            <p>HLS Video temporarily disabled because its too large for cloudflare workers</p>
+            {/*<HLSVideoElement onErrorCapture={(err) => {
             console.log('error playing hls video', err);
-          }} loop muted autoPlay src={url} slot="media" crossOrigin="anonymous" tabIndex={-1}></HLSVideoElement>
+          }} loop muted autoPlay src={url} slot="media" crossOrigin="anonymous" tabIndex={-1}></HLSVideoElement> */}
+          </div>
         ) : (
           <video loop muted autoPlay src={url} slot="media" crossOrigin="anonymous" tabIndex={-1}></video>
         )}
