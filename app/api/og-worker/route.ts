03637ea8322ebd75dbc9f73b0dcee50738ae2143
Wes Bos <wesbos@gmail.com>
2025-01-10 15:55:59 -0500

    migrate puppeteer to use local chrome instead

--- app/api/og-worker/route.ts
+++ app/api/og-worker/route.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getCloudflareContext } from '@opennextjs/cloudflare';
-import puppeteer, { Browser } from '@cloudflare/puppeteer';
-
+import { Browser } from '@cloudflare/puppeteer';
+import { getChromeWsEndpoint } from '@/utils/localChrome';
+const puppeteer = require('@cloudflare/puppeteer');
 let browser: Browser;
 
 async function getScreenshot(url: string) {
   const env = (await getCloudflareContext()).env;
   const kv = env.OG;
+  console.log(`env`, env);
   // first check if this value has been cached
   const cachedImage = await kv.get(url, 'arrayBuffer');
   if (cachedImage) {
@@ -16,7 +18,14 @@ async function getScreenshot(url: string) {
   console.log(`Launching browser`);
   console.log(`Cloudflare Browser:`, env.MYBROWSER);
   console.log(`KV Cache:`, kv);
-  browser = await puppeteer.launch(env.MYBROWSER);
+  if (env.MYBROWSER) {
+    // Cloudflare Browser
+    browser = await puppeteer.launch(env.MYBROWSER);
+  } else {
+    // Local Browser (for local development)
+    const browserWSEndpoint = await getChromeWsEndpoint();
+    browser = await puppeteer.connect({ browserWSEndpoint });
+  }
   console.log(`Browser launched`);
   const page = await browser.newPage();
   console.log(`Page created`);
@@ -24,13 +33,17 @@ async function getScreenshot(url: string) {
   console.log(`Viewport set`);
   await page.goto(url, { waitUntil: 'networkidle0' });
   console.log(`Page navigated`);
-  console.log(`Waiting for 1 second`);
   const buffer = await page.screenshot({ type: 'png' });
   console.log(`Screenshot taken`);
   // Cache to KV
-  await kv.put(url, buffer, {
-    expirationTtl: 60 * 60 * 24 * 30, // 30 days
-  });
+  // TODO Caching stopped working for some reason locally with miniflare.
+  try {
+    await kv.put(url, buffer, {
+      expirationTtl: 60 * 60 * 24 * 30, // 30 days
+    });
+  } catch (err) {
+    console.log(`Error caching image. Skipping`);
+  }
   console.log(`Put in KV cache`);
   console.log(`Returning buffer`);
   await browser.close();
@@ -43,6 +56,7 @@ export async function GET(request: NextRequest) {
   console.log(`Getting screenshot for ${url}`);
   const photoBuffer = await getScreenshot(url).catch(async (err) => {
     console.log(`Error getting screenshot`, err);
+    console.dir(err)
     await browser.close();
     throw err;
   });
