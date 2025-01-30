2631e500076466d481f6f822096422ca1af7e3c8
Wes Bos <wesbos@gmail.com>
2025-01-30 13:44:17 -0500

    fixes leaking global CSS for OG images

--- app/og/page.tsx
+++ app/og/page.tsx
@@ -2,7 +2,6 @@
 import React, { Suspense } from 'react';
 import H from '../../components/mdxComponents/Headings';
 import * as ogcss from '@/styles/OG.module.css';
-import '@/styles/ogGlobal.css';
 // import logo from '../assets/images/logo.png';
 import { useParams, usePathname, useSearchParams } from 'next/navigation';
 
@@ -27,33 +26,60 @@ function OG() {
   console.log(link);
   const linkURL = new URL(link);
 
-  // example URL: http://localhost:3000/thumbnail?title=Uses&url=https%3A%2F%2Fwesbos.com%2Fuses&thumbnail=%2F_next%2Fimage%3Furl%3D%252F_next%252Fstatic%252Fmedia%252Faeron.b514fb21.jpg%26w%3D1920%26q%3D75
+  // example URL: http://localhost:3000/og?title=Uses&url=https%3A%2F%2Fwesbos.com%2Fuses&thumbnail=%2F_next%2Fimage%3Furl%3D%252F_next%252Fstatic%252Fmedia%252Faeron.b514fb21.jpg%26w%3D1920%26q%3D75
   return (
-      <div className={ogcss.main}>
-        <div className={ogcss.title}>
-          <H
-            style={{
-              fontSize: getFontSize(title.length),
-              margin: 0,
-              lineHeight: 1.3,
-            }}
-          >
-            {title}
-          </H>
-          {thumbnail && <img className={ogcss.thumbnail} src={thumbnail} alt={title} />}
-        </div>
-        <div className={ogcss.lower}>
-          <img src="https://pbs.twimg.com/profile_images/877525007185858562/7G9vGTca_400x400.jpg" alt="Wes Bos" className={ogcss.handsome} />
-          <div>
-            <p className={ogcss.authorName}>Wes Bos.com</p>
-            <p className={ogcss.link}>
-              {/* <span className="host">{linkURL.host}</span> */}
-              <span className={ogcss.path}>{linkURL.pathname}</span>
-            </p>
-          </div>
-          <img src="/logo.png" alt="logo" className={ogcss.logo} />
-        </div>
+    <div className={ogcss.main}>
+      <div className={ogcss.title}>
+        <H
+          style={{
+            fontSize: getFontSize(title.length),
+            margin: 0,
+            lineHeight: 1.3,
+          }}
+        >
+          {title}
+        </H>
+        {thumbnail && <img className={ogcss.thumbnail} src={thumbnail} alt={title} />}
       </div>
+      <div className={ogcss.lower}>
+        <img src="https://pbs.twimg.com/profile_images/877525007185858562/7G9vGTca_400x400.jpg" alt="Wes Bos" className={ogcss.handsome} />
+        <div>
+          <p className={ogcss.authorName}>Wes Bos.com</p>
+          <p className={ogcss.link}>
+            {/* <span className="host">{linkURL.host}</span> */}
+            <span className={ogcss.path}>{linkURL.pathname}</span>
+          </p>
+        </div>
+        <img src="/logo.png" alt="logo" className={ogcss.logo} />
+      </div>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: /*css*/ `
+body {
+  position: relative;
+  z-index: -2;
+  margin: 0;
+  display: grid;
+  & > div,
+  & > div > div {
+    display: grid;
+  }
+  nav,
+  footer {
+    display: none !important;
+  }
+  /* Some global hacks to override the default styles */
+  [class*='ContentStyles'] {
+    width: 100%;
+    padding: 0;
+    max-width: none;
+  }
+}
+
+        `,
+        }}
+      />
+    </div>
   );
 }
 
