7cc91cf6f1dd2a3dd8590eb4c333a87b58224c19
Wes Bos <wesbos@gmail.com>
2025-02-07 11:24:48 -0500

    remove tunnel route

--- next.config.mjs
+++ next.config.mjs
@@ -87,7 +87,7 @@ const sentryConfig = {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: '/monitoring',
+  // tunnelRoute: '/monitoring',
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
