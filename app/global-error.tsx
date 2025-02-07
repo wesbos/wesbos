131b420deab0ac21c4ee438ea787d2b205c90d18
Wes Bos <wesbos@gmail.com>
2025-02-07 10:47:04 -0500

    global error page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -6,12 +6,14 @@ import { useEffect } from 'react';
 
 export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
+    console.log('Capturing Error with Sentry', error);
     Sentry.captureException(error);
   }, [error]);
 
   return (
     <html>
       <body>
+        <h2>Shoot, eh?</h2>
         {/* `NextError` is the default Next.js error page component. Its type
         definition requires a `statusCode` prop. However, since the App Router
         does not expose status codes for errors, we simply pass 0 to render a
