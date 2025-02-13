4eb132861c812bd3f3e55d66be6890f0550e8b1c
Wes Bos <wesbos@gmail.com>
2025-02-12 21:12:23 -0500

    toggle instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,13 +1,13 @@
-import * as Sentry from '@sentry/nextjs';
+// import * as Sentry from '@sentry/nextjs';
 
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
-  }
+// export async function register() {
+//   if (process.env.NEXT_RUNTIME === 'nodejs') {
+//     await import('./sentry.server.config');
+//   }
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
-  }
-}
+//   if (process.env.NEXT_RUNTIME === 'edge') {
+//     await import('./sentry.edge.config');
+//   }
+// }
 
-export const onRequestError = Sentry.captureRequestError;
+// export const onRequestError = Sentry.captureRequestError;
