07363c2e5c23936962d7cc36d413f630c49247c7
Wes Bos <wesbos@gmail.com>
2025-02-19 21:32:07 -0500

    waku hono cache

--- waku.config.ts
+++ waku.config.ts
@@ -1,34 +1,39 @@
 import type { Hono } from 'hono';
 import { type Config, defineConfig } from 'waku/config';
+import { cache } from 'hono/cache'
 
 export default defineConfig({
   ...(import.meta.env && !import.meta.env.PROD
     ? {
-        unstable_honoEnhancer: ((createApp: (app: Hono) => Hono) => {
-          const handlerPromise = import('./waku.cloudflare-dev-server').then(
-            ({ cloudflareDevServer }) =>
-              cloudflareDevServer({
-                // Optional config settings for the Cloudflare dev server (wrangler proxy)
-                // https://developers.cloudflare.com/workers/wrangler/api/#parameters-1
-                persist: {
-                  path: '.wrangler/state/v3',
-                },
-              }),
-          );
-          return (appToCreate: Hono) => {
-            const app = createApp(appToCreate);
-            return {
-              fetch: async (req: Request) => {
-                const devHandler = await handlerPromise;
-                return devHandler(req, app);
+      unstable_honoEnhancer: ((createApp: (app: Hono) => Hono) => {
+        const handlerPromise = import('./waku.cloudflare-dev-server').then(
+          ({ cloudflareDevServer }) =>
+            cloudflareDevServer({
+              // Optional config settings for the Cloudflare dev server (wrangler proxy)
+              // https://developers.cloudflare.com/workers/wrangler/api/#parameters-1
+              persist: {
+                path: '.wrangler/state/v3',
               },
-            };
+            }),
+        );
+        return (appToCreate: Hono) => {
+          const app = createApp(appToCreate);
+          return {
+            fetch: async (req: Request) => {
+              const devHandler = await handlerPromise;
+              return devHandler(req, app);
+            },
           };
-        }) as Config['unstable_honoEnhancer'],
-      }
+        };
+      }) as Config['unstable_honoEnhancer'],
+    }
     : {}),
   middleware: () => {
     return [
+      cache({
+        cacheName: 'waku-cache',
+        cacheControl: 'max-age=3600',
+      }),
       import('waku/middleware/context'),
       import('waku/middleware/dev-server'),
       import('./waku.cloudflare-middleware'),
