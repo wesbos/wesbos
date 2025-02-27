0a632d88e7d08981eda9e8ed39c43c160fd3f3ca
Wes Bos <wesbos@gmail.com>
2025-02-27 11:08:29 -0500

    prod

--- waku.config.ts
+++ waku.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
             }),
         );
         return (appToCreate: Hono) => {
-          // If you add routes before createApp, they are run before Waku.
+          // If you add routes before createApp, they are run before Waku. This could be handy for adding routing or middleware that isn't part of Waku.
           appToCreate.use('/wes', async (c, next) => {
             return new Response('Hello Wes');
           });
@@ -38,7 +38,22 @@ export default defineConfig({
         };
       }) as Config['unstable_honoEnhancer'],
     }
-    : {}),
+    : {
+      // Production config
+      unstable_honoEnhancer: ((createApp: (app: Hono) => Hono) => {
+        return (appToCreate: Hono) => {
+          appToCreate.use('/wes', async (c, next) => {
+            return new Response('Hello Wes from production');
+          });
+          const app = createApp(withSentry(appToCreate));
+          return {
+            fetch: async (req: Request) => {
+              return app.fetch(req);
+            },
+          };
+        };
+      }) as Config['unstable_honoEnhancer'],
+    }),
   middleware: () => {
     return [
       import('./src/middleware/og'),
