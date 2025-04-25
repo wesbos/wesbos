767463321f40fd5171fa3e5d0f56ed9bdaf86f2f
Wes Bos <wesbos@gmail.com>
2025-04-25 16:04:26 -0400

    Trying some new error logging

--- src/middleware/errorReporting.ts
+++ src/middleware/errorReporting.ts
@@ -5,7 +5,7 @@ const errorReportingMiddleware: Middleware = (
 ): ((ctx: HandlerContext, next: () => Promise<void>) => Promise<void>) => {
   options.unstable_onError.add((err: unknown, ctx: HandlerContext, origin: 'handler' | 'rsc' | 'html'): void => {
     console.log('Sent error to Sentry', Sentry.captureException(err));
-    console.log('STACK TRACE:')''
+    console.log('STACK TRACE:');
     console.error(err.stack);
     // capture it?
     const captured = Error.captureStackTrace(err);
